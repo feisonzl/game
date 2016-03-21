c97267cd0a024c7ee2ae042c26759b83aa5af574
feison <feison@feison-OptiPlex-3020.(none)>
2016-03-21 21:00:22 +0800

    update snake.c

--- snake.c
+++ snake.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <curses.h>
+
+
 typedef struct xy{
 	int x;
 	int y;
@@ -17,14 +20,41 @@ enum direction{
 	down,
 	left,
 	right,
-}
+};
+static dir[4][2]={
+    {1,0},
+    {-1,0},
+    {0,1},
+    {0,-1},
+};
 
+void gamewindow_init(int x,int y);
+snake_body *snake_init();
+snake_body *snake_tail(snake_body *snake_head);
+int getkeyvalue();
+void snake_show(snake_body *snake_head);
+int snake_add(snake_body* snake_head,food_xy* food_xy,direct*snake_dir,direct*keyboard_dir);
+void snake_free(snake_body* snake_head);
+int snake_rule(snake_body* snake_head,xy windows);
 
 
 int main()
 {
+    snake_body *snake_head=snake_init();
+    gamewindow_init(20,20);
+    mvaddch(10,10,'z');
+    mvaddstr(10,11,"zhanglei");
+    sleep(20000);
+    endwin();
+    snake_free(snake_head);
+    return 0;
 	
-	
+}
+
+void gamewindow_init(int x,int y)
+{
+    initscr();
+    box(stdscr,x,y); 
 }
 
 snake_body *snake_init()
@@ -79,22 +109,63 @@ int getkeyvalue()
 	return 0;	
 }
 
-void snake_show(snake_body *snake_init)
+void snake_show(snake_body *snake_head)
 {
-
+    snake_body * tmp=NULL;
+    tmp=snake_head;
+    for(;tmp!=NULL;tmp=tmp->next){
+        move(tmp->snake_xy.x,tmp->snake_xy.y);
+    }
+    return ;
 
 }
-void snake_add(snake_body* snake_head,food_xy* food_xy,direct*snake_dir,direct*keyboard_dir)
+int snake_add(snake_body* snake_head,food_xy* food_xy,direct*snake_dir,direct*keyboard_dir)
 {
 	int x,y;
 	snake_body* snake_tail=snake_tail(snake_head);
 	x=snake_head->xy_snake.x;
 	y=sanke_head->xy_snake.y;
-	if(food_xy.x==x&&food_xy.y==y){
-		if(*snakeidr=*keyborad_dir){
-			
-		}
-	}
-	
+	if(*snakeidr!=-*keyborad_dir){
+	    x=x+dir[*keyborad_dir][0];
+        y=y+dir[*keyborad_dir][1];	
+	}else{
+        printf("operation error!\n");
+        return -1;
+    }
+	return 0;
+}
+
+void snake_free(snake_body* snake_head)
+{
+    snake_body *tmp=NULL;
+    snake_body *free_tmp=NULL;
+    free_tmp=snake_head->next;
+    tmp=free_tmp->next;
+    while(tmp==NULL){
+        free(free_tmp);
+        free_tmp=tmp;    
+        tmp=free_tmp->next;
+    }
+    free(free_tmp);
+    return ;
+}
+int snake_rule(snake_body* snake_head,xy windows)
+{
+    int x,y;
+    snake_body* tmp=NULL;
+    x=snake_head->xy_snake.x;
+    y=snake_head->xy_snake.y;
+    if(x<0||y<0||x>windows.x||y>windows.y){
+        printf("game over!\n");
+        return -1;
+    }
+    tmp=snake_head->next;
+    for(;tmp!=NULL;tmp=tmp->next){
+        if(x==tmp->xy_snake.x&&y==tmp->snake.y){
+            printf("break rule!\n");
+            return -1;
+        }
+    }
+    return 0;
 }
 
